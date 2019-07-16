4b428b638f111eb304f3dd55516bea9350437d12
abc <abc@abc>
2019-07-16 09:34:04 +0700

    a.xtx

--- ERP.Database/Schema-Model/APPSYS/Tables/SystemConfiguration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [APPSYS].[SystemConfiguration]
-(
-[Key] [nvarchar] (50) NOT NULL,
-[Value] [nvarchar] (2000) NOT NULL
-)
-GO
-ALTER TABLE [APPSYS].[SystemConfiguration] ADD CONSTRAINT [PK_SystemConfiguration] PRIMARY KEY CLUSTERED  ([Key])
-GO
